ae833589227f3b4b4791f3391e99b44a43de559a
shrutivanw <shruti.dalvi@gmail.com>
2019-03-20 11:01:26 -0700

    Solution added
    Adding a solution in the solution branch of the factorial assignment

--- lib/factorial.rb
+++ lib/factorial.rb
@@ -1,6 +1,13 @@
 # Computes factorial of the input number and returns it
-# Time complexity: ?
-# Space complexity: ?
+# Time complexity: O(number) because the loop will get executed 'number' number of times
+# Space complexity: O(1) - fact auxiliary variable used
 def factorial(number)
-  raise NotImplementedError
+  raise ArgumentError.new("input cannot be nil") if number == nil
+
+  fact = 1
+  while number > 1
+    fact = fact * number
+    number -= 1
+  end
+  return fact
 end
